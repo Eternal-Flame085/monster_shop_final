e24ce34946cf1f1213868e4440b9f049c65ab61c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-11-07 15:39:12 -0500

    created index and routs for discounts

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,5 +1,27 @@
 class Merchant::DiscountsController < Merchant::BaseController
-  def new
 
+  def index
+    @discounts = Discount.all
+  end
+
+  def new
+    @discount = Discount.new
+  end
+
+  def create
+    merchant = current_user.merchant
+    @discount = merchant.discounts.new(discount_params)
+    if @discount.save
+      redirect_to '/merchant/discounts'
+    else
+      flash[:unsucessful] = @discount.errors.full_messages.uniq.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:name ,:quantity, :discount)
   end
 end
