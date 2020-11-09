5eb9ab2140fc447a448f63871a9765f44a0d23f8
Roberto Basulto <roberto.basulto085@gmail.com>
2020-11-08 20:56:34 -0500

    Added discount seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,7 @@ brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'D
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+brian.discounts.create(name: '5 percent', quantity: 5, discount: 5.0)
+brian.discounts.create(name: '10 percent', quantity: 10, discount: 10.0)
+megan.discounts.create(name: 'conflict 10 percent', quantity: 2, discount: 10.0)
+megan.discounts.create(name: 'conflict 15 percent', quantity: 2, discount: 15.0)
