eb4920c256e9154fe11ae1886c5d4e13923d87e8
Roberto Basulto <roberto.basulto085@gmail.com>
2020-11-08 20:35:10 -0500

    created model method test for .apply_discount()

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Cart do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @discount = @brian.discounts.create(name: '5 percent', quantity: 5, discount: 5.0)
+      @discount_2 = @brian.discounts.create(name: '10 percent', quantity: 10, discount: 10.0)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 2 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
@@ -62,6 +64,20 @@ RSpec.describe Cart do
       @cart.less_item(@giant.id.to_s)
 
       expect(@cart.count_of(@giant.id)).to eq(1)
+    end
+
+    it ".apply_discount()" do
+      5.times do
+        @cart.add_item(@hippo.id.to_s)
+      end
+
+      expect(@cart.apply_discount(@hippo)).to eq(@discount)
+
+      5.times do
+        @cart.add_item(@hippo.id.to_s)
+      end
+
+      expect(@cart.apply_discount(@hippo)).to eq(@discount_2)
     end
   end
 end
